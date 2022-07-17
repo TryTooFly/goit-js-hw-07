1d0451248820e7c5a59efcb587a2a1672f28ea14
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-07-17 15:23:30 +0300

    task2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join("");
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
