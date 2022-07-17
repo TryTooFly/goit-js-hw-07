f29e5146822639827720e4eeb1442c50b9695ffb
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-07-17 21:45:22 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryContainer = document.querySelector(".gallery");
-function photoCardMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
+
+const photoCardMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
     <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -15,27 +15,55 @@ function photoCardMarkup(galleryItems) {
     />
   </a>
 </div>`;
-    })
-    .join("");
-}
-const cardsMarkup = photoCardMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
-galleryContainer.addEventListener("click", openImageClick);
+  })
+  .join("");
 
-function openImageClick(e) {
+// const cardsMarkup = photoCardMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+// galleryContainer.addEventListener("click", openImageClick);
+
+// function openImageClick(e) {
+//   e.preventDefault();
+
+//   const modal = basicLightbox.create(
+//     `<img scr= "${e.target.dataset.source}" width="800" height="600">`,
+//     {
+//       onShow: (modal) => {
+//         window.addEventListener("keydown", onPressKeyESC);
+//       },
+//     },
+//     {
+//       onclose: (modal) => {
+//         window.removeEventListener("keydown", onPressKeyESC);
+//       },
+//     }
+//   );
+galleryContainer.innerHTML = photoCardMarkup;
+
+let imgOriginalLink = "";
+
+galleryContainer.addEventListener("click", (e) => {
   e.preventDefault();
 
+  imgOriginalLink = e.target.dataset.source;
+
   const modal = basicLightbox.create(
-    `<img scr= "${e.target.dataset.source}" width="800" height="600">`,
+    `
+      <img src="${imgOriginalLink}">`,
     {
-      onShow: () => window.addEventListener("keydown", onPressKeyESC),
-      onclose: () => window.removeEventListener("keydown", onPressKeyESC),
+      onShow: (modal) => {
+        window.addEventListener("keydown", openModal);
+      },
+      onClose: (modal) => {
+        window.removeEventListener("keydown", openModal);
+      },
     }
   );
+
   modal.show();
-  function onPressKeyESC(e) {
+  function openModal(e) {
     if (e.code === "Escape") {
       modal.close();
     }
   }
-}
+});
