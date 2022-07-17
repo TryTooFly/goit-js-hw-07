0c002532339a80cc44740d038d33cb55b260b94c
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-07-17 14:58:26 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+function photoCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+const cardsMarkup = photoCardMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", openImageClick);
 
-console.log(galleryItems);
+function openImageClick(e) {
+  e.preventDefault();
+  const modal = basicLightbox.create(
+    `<img scr='${e.target.dataset.source}' width='800' height='600'>`,
+    {
+      onShow: () => window.addEventListener("keydown", onPressKeyESC),
+      onclose: () => window.removeEventListener("keydown", onPressKeyESC),
+    }
+  );
+  modal.show();
+  function onPressKeyESC(e) {
+    if (e.code === "Escape") {
+      modal.close();
+    }
+  }
+}
